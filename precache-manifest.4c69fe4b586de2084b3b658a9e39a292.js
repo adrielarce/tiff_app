50b8ca58dee5758e214469dbf3dd0d052ebcfa4e
Adriel Arce <adrielarce@Adriels-MacBook-Pro.local>
2019-05-17 21:48:28 -0400

    Updates

---
+++ precache-manifest.4c69fe4b586de2084b3b658a9e39a292.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d78bbb193d4d4762b51361f72fd47675",
+    "url": "/adrielarce/tiff_app/index.html"
+  },
+  {
+    "revision": "22483f851c26d966904f",
+    "url": "/adrielarce/tiff_app/static/css/main.50d0efa8.chunk.css"
+  },
+  {
+    "revision": "214efbc2993fbfe0c010",
+    "url": "/adrielarce/tiff_app/static/js/2.5f7ff4b1.chunk.js"
+  },
+  {
+    "revision": "22483f851c26d966904f",
+    "url": "/adrielarce/tiff_app/static/js/main.c31d3f8f.chunk.js"
+  },
+  {
+    "revision": "73bdfc3494728ac9c80f",
+    "url": "/adrielarce/tiff_app/static/js/runtime~main.0c9997eb.js"
+  }
+]);
